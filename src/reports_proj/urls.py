ae861b7102464403c111dc8e52fa1cfb87992437
Albert <albert@DESKTOP-43VQ7BP.localdomain>
2022-06-16 17:07:41 +0800

    Change styling to improve readability

--- src/reports_proj/urls.py
+++ src/reports_proj/urls.py
@@ -21,13 +21,18 @@ from .views import login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+# Class-based views
     path('', include('sales.urls', namespace='sales')),
     path('reports/', include('reports.urls', namespace='reports')),
     path('my_profile/', include('profiles.urls', namespace='profiles')),
+
+# Function views for login and logout
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
 
 ]
 
+# Static urls and the corresponding static directories
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
