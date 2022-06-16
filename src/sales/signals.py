3f8d813d53c37773588f2e4cec88370ffc23e895
Albert <albert@DESKTOP-43VQ7BP.localdomain>
2022-06-16 08:28:41 +0800

    Add sales data to database.

--- src/sales/signals.py
+++ src/sales/signals.py
@@ -1,7 +1,10 @@
-from .models import Sale
+from sales.models import Sale
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 
+
+from customers.models import Customer
+from products.models import Product
 
 @receiver(m2m_changed, sender=Sale.positions.through)
 def calculate_total_price(sender, instance, action, **kwargs):
@@ -14,3 +17,24 @@ def calculate_total_price(sender, instance, action, **kwargs):
 
     instance.total_price = total_price
     instance.save()
+
+def insert_record(rec, table):
+
+    # Check if record exists in table
+    try:
+        rec = table.objects.get(name=rec)
+    except table.DoesNotExist:
+        rec = None
+
+    # if record exists no action is necessary
+    # if record does not exist insert new record
+    return
+
+    if rec == None:
+        try:
+            create_rec = table.objects.create(name=rec)
+        except:
+            print ("Error creating customer record")
+    print ("Record created successfully")
+    return
+
